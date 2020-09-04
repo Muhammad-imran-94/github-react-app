29d52333016bd6ac2789f2b7ad585e6c94aa069f
unknown <imran101mughal@gmail.com>
2020-09-05 01:57:45 +0500

    Updates

---
+++ precache-manifest.e3063917700a59c4c2bbabb9ad5282d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5629944774cbf7bdc09ae87a5bc57f8",
+    "url": "/github-react-app/index.html"
+  },
+  {
+    "revision": "4cac84f9071508219cb9",
+    "url": "/github-react-app/static/css/2.4407c9ab.chunk.css"
+  },
+  {
+    "revision": "d45a775b72ff3e3d070d",
+    "url": "/github-react-app/static/css/main.131d267e.chunk.css"
+  },
+  {
+    "revision": "4cac84f9071508219cb9",
+    "url": "/github-react-app/static/js/2.68a408ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-react-app/static/js/2.68a408ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d45a775b72ff3e3d070d",
+    "url": "/github-react-app/static/js/main.15c89ddd.chunk.js"
+  },
+  {
+    "revision": "3b6fce5a97affbb9eb95",
+    "url": "/github-react-app/static/js/runtime-main.532f07f6.js"
+  }
+]);
